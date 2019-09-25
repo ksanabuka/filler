8cbf0b2ed5b888edd81c52f262d765e87b6fe4ee
ksanabuka <obuksha@hotmail.com>
2019-09-25 16:19:04 -0700

    unknown error with output

--- main.c
+++ main.c
@@ -5,6 +5,12 @@
 //  Created by Oksana Buksha on 7/21/19.
 //  Copyright © 2019 Oksana Buksha. All rights reserved.
 //
+// gcc -Wall -Werror -Wextra -g -fsanitize=address ./inc/gnl/get_next_line.c main.c solver.c selecting_hod.c -L ./inc/gnl/libft -lft -L ./inc/ft_printf -lftprintf -I ./inc/ft_printf/parser.h   -I./inc/gnl -I./inc/gnl/libft -o obuksha.filler
+//./filler_vm -f maps/map00 -p1 players/abanlin.filler -p2 players/champely.filler -sjk
+// gcc -Wall -Werror -Wextra -g -o obuksha.filler *.c
+
+//./filler_vm -f maps/map00 -p2 players/abanlin.filler -p1 ../obuksha.filler  | less
+// gcc -Wall -Werror -Wextra -g -o obuksha.filler *.c
 
 #include "arr2d.h"
 #include "player.h"
@@ -20,12 +26,20 @@
 
 void write_option(t_point p, int fd);
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
+    argc = 0;
+    argv = 0; 
     t_player me;
-    int in_fd = 0;
+   int in_fd = 0;
+    
+    // int in_fd;
+    // in_fd = open("test.txt", O_RDONLY);
+
     me = read_player(in_fd);
     
-    while (1) {
+    while (1) 
+    {
         t_arr2d map = read_map(in_fd);
         t_arr2d piece = read_piece(in_fd);
         t_point p = evaluate_best_move(map, piece, me);
@@ -33,10 +47,12 @@ int main(int argc, const char * argv[]) {
         arr2dDispose(piece);
         arr2dDispose(map);
     }
+    // close(in_fd);
     return 0;
 }
 
-void write_option(t_point p, int fd) {
+void write_option(t_point p, int fd) 
+{
     char str[16];
     i_to_str(p.row, str);
     write(fd, str, str_len(str));
--- read_2darr.c
+++ read_2darr.c
@@ -8,6 +8,10 @@
 
 #include "read_2darr.h"
 #include "utils.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 static void read_arr_lines(int fd, struct arr2d arr, char *buf, int is_map) {
     int r;
@@ -22,9 +26,11 @@ static void read_arr_lines(int fd, struct arr2d arr, char *buf, int is_map) {
         read_line(fd, buf);
         c = 0;
         
-        while (c < arr.num_cols) {
+        while (c < arr.num_cols) 
+        {
             map_char = buf[c + (is_map ? 4 : 0)];
-            if (map_char >= 'A' && map_char <= 'Z') {
+            if (map_char >= 'A' && map_char <= 'Z') 
+            {
                 map_char = map_char - 'A' + 'a';
             }
             arr2dSetItemAt(arr, map_char, r, c);
@@ -34,7 +40,51 @@ static void read_arr_lines(int fd, struct arr2d arr, char *buf, int is_map) {
     }
 }
 
-struct arr2d read_arr2d(int fd, const char *dims_prefix, int is_map) {
+//  static void my_read_arr_lines(int fd, struct arr2d arr, int is_map) 
+//  {
+//     int r;
+//     int c;
+//     char map_char;
+//     int count;
+//     int num_cells;
+//     char * buf;
+
+//     num_cells = (is_map) ? (arr.num_cols + 4 + 1) * (arr.num_rows + 1) : (arr.num_cols + 1) * arr.num_rows; 
+//     buf = (char*)malloc(sizeof(char) * (num_cells + 1)); 
+//     count = read(fd, buf, num_cells);
+//     printf("num_cells - %d\n bits - %d,\n string:\n%s\n", num_cells, count, buf); 
+//     buf[num_cells] = '\0';
+   
+//     int i; 
+   
+//     i = (is_map) ? arr.num_cols + 1 + 4: 0; // skip first row in plateu; 
+//     r = 0; 
+//     while (r < arr.num_rows)
+//     {
+//         i += (is_map) ? 4 : 0;
+//         c = 0;
+//         while (c < arr.num_cols)
+//         {
+//             map_char = buf[i];
+//             if (map_char >= 'A' && map_char <= 'Z') 
+//             {
+//                 map_char = map_char - 'A' + 'a';
+//             }
+//             arr2dSetItemAt(arr, map_char, r, c);
+//             c++; 
+//             i++;
+//         }
+//         i++; 
+//         r++;
+
+//     }
+
+//     free(buf);
+// }
+
+
+struct arr2d read_arr2d(int fd, const char *dims_prefix, int is_map) 
+{
     struct arr2d res;
     char buf[1024];
     const char *dims_ptr;
@@ -54,6 +104,28 @@ struct arr2d read_arr2d(int fd, const char *dims_prefix, int is_map) {
     read_arr_lines(fd, res, buf, is_map);
     return res;
 }
+
+// struct arr2d read_arr2d(int fd, const char *dims_prefix, int is_map) 
+// {
+//     struct arr2d res;
+//     char buf[1024];
+//     const char *dims_ptr;
+//     int rows;
+//     int cols;
+//     read_line(fd, buf);
+    
+//     if (!str_starts_with(buf, dims_prefix)) {
+//         app_error("arr dimensions error");
+//     }
+    
+//     dims_ptr = buf + str_len(dims_prefix);
+//     rows = str_to_i(dims_ptr);
+//     dims_ptr = str_chr(dims_ptr, ' ') + 1;
+//     cols = str_to_i(dims_ptr);
+//     res = createArr2d(rows, cols);
+//     my_read_arr_lines(fd, res, is_map);
+//     return res;
+// }
 
 struct arr2d read_map(int fd) {
     return read_arr2d(fd, "Plateau ", 1 /* is_map */);
